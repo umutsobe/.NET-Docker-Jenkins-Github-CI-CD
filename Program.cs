b172f5f414dd3afe9ab329ac8ac9e7b98d462f10
umutsobe <umutsobee@gmail.com>
2023-10-15 01:42:20 +0300

    port changed

--- Program.cs
+++ Program.cs
@@ -2,12 +2,16 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.ListenAnyIP(1000); // to listen for incoming http connection on port 5001
+});
+
 builder.Services.AddControllers();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.WebHost.UseUrls("http://*:1000");
 
 var app = builder.Build();
 
@@ -24,4 +28,4 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-app.Run("http://localhost:1000");
+app.Run();
